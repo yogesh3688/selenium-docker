94a9af92a5282baab545da28d8092b92e3a8ad01
Yogesh Nipte <yogeshgnipate@yahoo.com>
2025-07-24 22:35:26 +0530

    updated runner file

--- runner.sh
+++ runner.sh
@@ -21,19 +21,21 @@ echo "Checking if hub is ready..!"
 echo "hub_host is ---> "
 echo ${HUB_HOST}
 
-count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
-  count=$((count+1))
-  echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
-  fi
-  sleep 1
-done
+#count=0
+#while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+#do
+#
+#  count=$((count+1))
+#  echo "Attempt: ${count}"
+#  if [ "$count" -ge 30 ]
+#  then
+#      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+#      exit 1
+#  fi
+#  sleep 2
+#done
 
+sleep 20
 # At this point, selenium grid should be up!
 echo "Selenium Grid is up and running. Running the test...."
 
